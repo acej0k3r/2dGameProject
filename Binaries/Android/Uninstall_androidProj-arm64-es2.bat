e7966ef427af02f4b2417f182dc26dbeeeedf6ea
Md A FaIsal <42229840+acej0k3r@users.noreply.github.com>
2018-12-28 19:15:22 -0500

    Add files via upload
    binary files are here

---
+++ Binaries/Android/Uninstall_androidProj-arm64-es2.bat
@@ -0,0 +1,18 @@
+setlocal
+set ANDROIDHOME=%ANDROID_HOME%
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/NVPACK/android-sdk-windows
+set ADB=%ANDROIDHOME%\platform-tools\adb.exe
+set DEVICE=
+if not "%1"=="" set DEVICE=-s %1
+for /f "delims=" %%A in ('%ADB% %DEVICE% shell "echo $EXTERNAL_STORAGE"') do @set STORAGE=%%A
+@echo.
+@echo Uninstalling existing application. Failures here can almost always be ignored.
+%ADB% %DEVICE% uninstall com.PhoenixCore.androidProj
+@echo.
+echo Removing old data. Failures here are usually fine - indicating the files were not on the device.
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/androidProj
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/UE4CommandLine.txt
+%ADB% %DEVICE% shell rm -r %STORAGE%/obb/com.PhoenixCore.androidProj
+%ADB% %DEVICE% shell rm -r %STORAGE%/Android/obb/com.PhoenixCore.androidProj
+@echo.
+@echo Uninstall completed
